b9e81ae03c20542e93e49d0dcbe7a288fa5da45f
vered bauer <veredbr2000@yahoo.com>
2012-12-25 16:01:27 -0600

    email validations and tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -17,4 +17,31 @@ describe User do
     no_name_user = User.new(@attr.merge(:name => ""))
     no_name_user.should_not be_valid
   end
+
+  it "should require an email" do
+    no_email_user = User.new(@attr.merge(:email => ""))
+    no_email_user.should_not be_valid
+  end
+
+  it "should reject names that are too long" do
+    long_name = "a" * 51
+    long_name_user = User.new(@attr.merge(:name => long_name))
+    long_name_user.should_not be_valid
+  end
+
+  it "should accept valid email addresses" do
+    addresses = %w[user@foo.com The_user@foo.bar.org first.last@foo.jp]
+    addresses.each do |address|
+        valid_email_user = User.new(@attr.merge(:email => address))
+        valid_email_user.should be_valid
+    end
+  end
+
+  it "should reject invalid addresses" do
+    addresses = %w[user.foo.com user,foo.com user@foo]
+    addresses.each do |address|
+        invalid_email_user = User.new(@attr.merge(:email => address))
+        invalid_email_user.should_not be_valid
+    end
+  end
 end
