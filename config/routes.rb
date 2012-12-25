459872e64c217f20a7ea820ecac0967e1c4ccdb9
vered bauer <veredbr2000@yahoo.com>
2012-12-24 22:03:05 -0600

    integration tests

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ NewProject::Application.routes.draw do
   get "users/new"
   match '/signup', :to => 'users#new'
   get "pages/home"
-
   get "pages/contact"
-
+  get "pages/about"
+  match '/contact', :to => 'pages#contact'
+  match '/about', :to => 'pages#about'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
