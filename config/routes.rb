61a20cee180d96d127ce18b212ab58f62fcc4497
vered bauer <veredbr2000@yahoo.com>
2013-01-01 22:21:43 -0600

    adding Sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 NewProject::Application.routes.draw do
 
+  get "sessions/new"
+
   resources :users
+  resources :sessions, :only => [:new, :create, :destroy]
   get "users/new"
   match '/signup', :to => 'users#new'
+  match '/signin', :to => 'sessions#new'
+  match '/signout', :to => 'sessions#destroy'
   get "pages/home"
   get "pages/contact"
   get "pages/about"
